f4563a36f88b5a573b21dc89f155b28ca044e293
Andrew <62225671+areeson@users.noreply.github.com>
2022-04-11 15:47:38 -0500

    added certs

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ setInterval(() => {
 
     timeEl.innerHTML = (hoursIn12HrFormat < 10? '0'+hoursIn12HrFormat : hoursIn12HrFormat) + ':' + (minutes < 10? '0' + minutes : minutes) + ' ' + `<span id="am-pm">${ampm}</span>`
 
-    dateEl.innerHTML = days[day] + ', ' + date + ' ' + months[month]
+    dateEl.innerHTML = days[day] + ', ' + months[month] + ' ' + date
 
 }, 1000);
 
